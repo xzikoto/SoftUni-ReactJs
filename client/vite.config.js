383153992009fc3693b328ed02445bfc7cbab8e2
Ivailo G. <ivailogramatikov05@gmail.com>
2024-08-07 23:49:53 +0300

    Fix Google oAuth

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ["@react-oauth/google", "lottie-web"],
-    },
+    rollupOptions: {},
   },
 });
