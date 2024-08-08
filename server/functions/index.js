6571e7d3a142787b886f66e90277416794ed46bb
Ivailo G. <ivailogramatikov05@gmail.com>
2024-08-08 07:24:52 +0300

    Minor Bugs Fixes

--- server/functions/index.js
+++ server/functions/index.js
@@ -4,10 +4,6 @@ const { onRequest } = require("firebase-functions/v2/https");
 const { setGlobalOption } = require("firebase-functions/v2");
 const logger = require("firebase-functions/logger");
 
-// setGlobalOption({
-//   region: "europe-west1",
-// });
-
 exports.api = onRequest((req, res) => {
   server.emit("request", req, res);
 });
